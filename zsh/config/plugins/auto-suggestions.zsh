09d7ebe6df02583f216ad4ff50b8c94684d1952e
phhphc <phhphc@gmail.com>
2024-08-19 19:56:26 +0700

    zsh config

--- zsh/config/plugins/auto-suggestions.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-__hb_asg_handler="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-if [[ -f "$__hb_asg_handler" ]]; then 
-  source "$__hb_asg_handler"
-fi 
--- zsh/config/plugins/syntax-highlighting.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-__hb_shl_handler="$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-if [[ -f "$__hb_shl_handler" ]]; then 
-  source "$__hb_shl_handler"
-fi 
