e912d41c5b302ad1f2fde18a0b1014ad86a23c3f
Pham Phuoc <phhphc@192.168.1.42.non-exists.ptr.local>
2025-04-05 01:30:19 +0700

    add auto install script

---
+++ brew-tools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+brew install --cask firefox
+
+brew install --cask karabiner-elements
+brew install --cask raycast
+
+brew install --cask kitty
+
+brew install --cask jetbrains-toolbox
+
+brew install --cask evkey
+
+# terminal tool
+brew install yazi neovim
+
+# diff view tool
+brew install difftastic
+
+# development tool
+brew install oven-sh/bun/bun node
+brew install go
+brew install rust
