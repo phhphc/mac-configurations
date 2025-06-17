649ed2792c3964c954bb269bcba22532391f7a72
phuocph3 <phuocph3@vng.com.vn>
2025-06-17 09:52:48 +0700

    use neo-tree

--- nvim/config-v2/lua/plugins/telescope.lua
+++ nvim/config-v2/lua/plugins/telescope.lua
@@ -8,22 +8,22 @@ return {
 		cmd = { "Telescope" },
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Find buffers" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
 		},
 
 		-- TODO: install this plugin:
 		-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
 	},
 
-	{
-		lazy = false,
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").load_extension("file_browser")
-		end,
-		keys = {
-			{ "<leader>e", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", desc = "File browser" },
-		},
-	},
+	-- {
+	-- 	lazy = false,
+	-- 	"nvim-telescope/telescope-file-browser.nvim",
+	-- 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	-- 	config = function()
+	-- 		require("telescope").load_extension("file_browser")
+	-- 	end,
+	-- 	keys = {
+	-- 		{ "<leader>e", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", desc = "File browser" },
+	-- 	},
+	-- },
 }
