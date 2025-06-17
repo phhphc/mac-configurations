e8b9447dca0bd3e5262a689a7a55bf414ad370df
phuocph3 <phuocph3@vng.com.vn>
2025-06-17 09:18:08 +0700

    refactor

--- nvim/config-v2/lua/languages/lua.lua
+++ nvim/config-v2/lua/languages/lua.lua
@@ -1,7 +1,9 @@
 ---@type LazySpec
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = function(_, opts)
-		opts.ensure_installed = table.insert(opts.ensure_installed, "lua_ls")
-	end,
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = table.insert(opts.ensure_installed, "lua_ls")
+		end,
+	},
 }
