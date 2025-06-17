bc38ed9fbc10cabe78c44a01b73482bf8668ac23
phuocph3 <phuocph3@vng.com.vn>
2025-06-17 09:28:34 +0700

    refactor

--- nvim/config-v2/lua/languages/lua.lua
+++ nvim/config-v2/lua/languages/lua.lua
@@ -3,7 +3,7 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = function(_, opts)
-			opts.ensure_installed = table.insert(opts.ensure_installed, "lua_ls")
+			opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "lua_ls" })
 		end,
 	},
 }
