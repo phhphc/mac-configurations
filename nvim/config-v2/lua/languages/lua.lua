5ebff3cccc0c35dc22e749735796d82a66eb74b1
phuocph3 <phuocph3@vng.com.vn>
2025-06-17 09:17:07 +0700

    fix bug

--- nvim/config-v2/lua/languages/lua.lua
+++ nvim/config-v2/lua/languages/lua.lua
@@ -2,6 +2,6 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = function(_, opts)
-		table.insert(opts.ensure_installed, "lua_ls")
+		opts.ensure_installed = table.insert(opts.ensure_installed, "lua_ls")
 	end,
 }
