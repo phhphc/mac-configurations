0515cffbd053aafb42ced419ee2af8dcfa4aaa80
phhphc <phhphc@gmail.com>
2025-06-20 00:30:06 +0700

    update config

--- nvim/config-v2/lua/pre-config.lua
+++ nvim/config-v2/lua/pre-config.lua
@@ -21,3 +21,8 @@ else
 
 	vim.opt.winborder = "rounded"
 end
+
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save buffer" })
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", { desc = "Quit window" })
+vim.keymap.set("n", "<leader>Q", "<cmd>quitall<cr>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<leader>n", "<cmd>nohlsearch<cr>", { desc = "No hightlight search" })
