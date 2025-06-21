e950aab81175303013a7408d0fd605cc8bb60afd
phhphc <phhphc@gmail.com>
2025-06-21 21:13:42 +0700

    add spelling

---
+++ nvim/astronvim_v5/lua/plugins/spelling.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = {
+        spell = false,
+        spelllang = "en,vi",
+      },
+    },
+  },
+}
