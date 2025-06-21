12c371c3265c7aab50f8d564228d9f262598a002
phhphc <phhphc@gmail.com>
2025-06-22 00:18:29 +0700

    refactor

--- nvim/config/lua/polish.lua
+++ nvim/config/lua/polish.lua
@@ -1,5 +1,9 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- remove neovim default lsp key binding
+vim.keymap.del("n", "grr")
+vim.keymap.del("n", "grn")
+vim.keymap.del({ "n", "x" }, "gra")
+vim.keymap.del("n", "gri")
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+-- config spell
+vim.opt.spell = false
+vim.opt.spelllang = "en,vi"
