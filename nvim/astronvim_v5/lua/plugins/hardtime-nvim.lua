d106dcae6fff6c796db290bad0e8b721b89584c9
phhphc <phhphc@gmail.com>
2025-06-21 21:07:00 +0700

    update hardtime config

--- nvim/astronvim_v5/lua/plugins/hardtime-nvim.lua
+++ nvim/astronvim_v5/lua/plugins/hardtime-nvim.lua
@@ -3,5 +3,12 @@ return {
   "m4xshen/hardtime.nvim",
   event = "VeryLazy",
   dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {},
+  opts = {
+    disabled_keys = {
+      ["<Up>"] = false,
+      ["<Down>"] = false,
+      ["<Left>"] = false,
+      ["<Right>"] = false,
+    },
+  },
 }
