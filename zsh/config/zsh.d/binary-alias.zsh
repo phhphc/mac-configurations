1aa8b4ebd5de06df8d78626fa3e8b902edd8072a
phhphc <phhphc@gmail.com>
2025-06-21 17:28:36 +0700

    add astronvim 5 alias

--- zsh/config/zsh.d/binary-alias.zsh
+++ zsh/config/zsh.d/binary-alias.zsh
@@ -1,16 +1,13 @@
 #!/bin/zsh
 
-
 # ls alias
 alias ll='ls -l'
 alias la='ls -A'
 alias l='ls -CF'
 
-
 # launch alias
 alias it="open -a Kitty ."
 alias ui="open -a Finder ."
-
 
 # binary alias
 alias b='bun'
@@ -18,4 +15,4 @@ alias n='node'
 alias p='python'
 alias v='nvim'
 alias v2='NVIM_APPNAME=nvim2 nvim'
-
+alias v5='NVIM_APPNAME=astronvim_v5 nvim'
