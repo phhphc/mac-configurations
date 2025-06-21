b7b89cbb38e5edc5874d4789b65257c0faf9c048
phhphc <phhphc@gmail.com>
2025-06-21 19:05:03 +0700

    add neotree cmd

--- nvim/astronvim_v5/lua/plugins/user.lua
+++ nvim/astronvim_v5/lua/plugins/user.lua
@@ -1,3 +1,19 @@
+---@type LazySpec[]
 return {
   { "max397574/better-escape.nvim", enabled = false },
+  {
+    -- disable neovim default lsp keybinding
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["grr"] = false,
+          ["grn"] = false,
+          ["gra"] = false,
+          ["gri"] = false,
+        },
+      },
+    },
+  },
 }
