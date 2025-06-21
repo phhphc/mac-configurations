74017f720dfa4fd3d0e8405cba6ac621546b4f7f
phhphc <phhphc@gmail.com>
2025-06-21 20:25:48 +0700

    add plugin

---
+++ nvim/astronvim_v5/lua/plugins/nvim-surround.lua
@@ -0,0 +1,7 @@
+---@type LazySpec
+return {
+  "kylechui/nvim-surround",
+  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  opts = {},
+}
