cdd278936cb4b111cc60fe0d5d763e0b34ee6aad
phhphc <phhphc@gmail.com>
2025-06-21 21:46:05 +0700

    add kitty integration

---
+++ nvim/astronvim_v5/lua/plugins/kitty.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  {
+    -- kitty scrollback buffer
+    "mikesmithgh/kitty-scrollback.nvim",
+    lazy = true,
+    version = "^5.0.0",
+    cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+    event = { "User KittyScrollbackLaunch" },
+    config = function() vim.opt.showtabline = 0 end,
+  },
+  -- kitty & nvim seamless navigate
+  -- { "mrjones2014/smart-splits.nvim", build = "./kitty/install-kittens.bash" },
+}
