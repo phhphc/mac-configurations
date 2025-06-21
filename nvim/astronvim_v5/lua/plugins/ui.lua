eeae59c81187915104ba8f2612f2e36527c17fba
phhphc <phhphc@gmail.com>
2025-06-21 21:36:32 +0700

    update ui setting

--- nvim/astronvim_v5/lua/plugins/ui.lua
+++ nvim/astronvim_v5/lua/plugins/ui.lua
@@ -1,11 +1,25 @@
----@type LazySpec
+---@type LazySpec[]
 return {
+  -- appearence
+  { import = "astrocommunity.recipes.disable-tabline" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+
+  -- colorscheme
   { import = "astrocommunity.colorscheme.catppuccin" },
   {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
     opts = {
       colorscheme = "catppuccin-mocha",
+      highlights = {
+        ["catppuccin-mocha"] = {
+          SpellBad = {
+            undercurl = true,
+            sp = "#a6e3a1",
+          },
+        },
+      },
     },
   },
 }
