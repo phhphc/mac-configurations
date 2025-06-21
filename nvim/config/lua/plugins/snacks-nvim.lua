d0fe697083a719b00404882f97c5c1a23e1884e8
phhphc <phhphc@gmail.com>
2025-06-22 00:37:08 +0700

    refactor

--- nvim/config/lua/plugins/snacks-nvim.lua
+++ nvim/config/lua/plugins/snacks-nvim.lua
@@ -11,11 +11,11 @@ return {
     opts = {
       mappings = {
         n = {
-          ["gd"] = { function() require("snacks").picker.lsp_definitions() end, desc = "Goto Definition" },
-          ["gD"] = { function() require("snacks").picker.lsp_declarations() end, desc = "Goto Declaration" },
-          ["gr"] = { function() require("snacks").picker.lsp_references() end, nowait = true, desc = "References" },
-          ["gi"] = { function() require("snacks").picker.lsp_implementations() end, desc = "Goto Implementation" },
-          ["gy"] = { function() require("snacks").picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+          ["gd"] = { function() require("snacks.picker").lsp_definitions() end, desc = "Goto Definition" },
+          ["gD"] = { function() require("snacks.picker").lsp_declarations() end, desc = "Goto Declaration" },
+          ["gr"] = { function() require("snacks.picker").lsp_references() end, nowait = true, desc = "References" },
+          ["gi"] = { function() require("snacks.picker").lsp_implementations() end, desc = "Goto Implementation" },
+          ["gy"] = { function() require("snacks.picker").lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
         },
       },
     },
