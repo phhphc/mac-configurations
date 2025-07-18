93564e4b6ae4d4942a274902420751fa23c37dbf
phuocph3 <phuocph3@vng.com.vn>
2025-07-18 09:40:21 +0700

    add pomodoro

--- hammerspoon/config/init.lua
+++ hammerspoon/config/init.lua
@@ -1,4 +1,5 @@
-hs.alert.show("New config reloaded")
--- hs.loadSpoon("EmmyLua")
+hs.alert.show "New config reloaded"
+hs.loadSpoon "EmmyLua"
 
-require("src.window_focus")
+require "src.window_focus"
+require "src.pomodoro_timer"
---
+++ hammerspoon/config/src/pomodoro_timer.lua
@@ -0,0 +1,4 @@
+hs.loadSpoon "Cherry"
+
+Cherry = spoon.Cherry
+Cherry.duration = 15
