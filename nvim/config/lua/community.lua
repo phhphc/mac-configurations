8003155c29c3d11185883890a753e9cedd2356a2
phuocph3 <phuocph3@vng.com.vn>
2025-10-17 15:26:18 +0700

    disable nvim java because it is quite buggy

--- nvim/config/lua/community.lua
+++ nvim/config/lua/community.lua
@@ -12,7 +12,7 @@ return {
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
