61b7689253ae32a0615017972c0ae19d6745cbaf
phuocph3 <phuocph3@vng.com.vn>
2025-10-17 11:58:56 +0700

    disable hardtime

--- nvim/config/lua/plugins/hardtime-nvim.lua
+++ nvim/config/lua/plugins/hardtime-nvim.lua
@@ -1,16 +1,18 @@
----@type LazySpec
-return {
-  "m4xshen/hardtime.nvim",
-  event = "VeryLazy",
-  dependencies = { "MunifTanjim/nui.nvim" },
-  opts = {
-    max_time = 500,
-    max_count = 15,
-    disabled_keys = {
-      ["<Up>"] = false,
-      ["<Down>"] = false,
-      ["<Left>"] = false,
-      ["<Right>"] = false,
-    },
-  },
-}
+return {}
+
+-- ---@type LazySpec
+-- return {
+--   "m4xshen/hardtime.nvim",
+--   event = "VeryLazy",
+--   dependencies = { "MunifTanjim/nui.nvim" },
+--   opts = {
+--     max_time = 500,
+--     max_count = 15,
+--     disabled_keys = {
+--       ["<Up>"] = false,
+--       ["<Down>"] = false,
+--       ["<Left>"] = false,
+--       ["<Right>"] = false,
+--     },
+--   },
+-- }
