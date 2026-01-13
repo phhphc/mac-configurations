262392982474f0fb6e5eb27668140da931e308fa
phhphc <phhphc@gmail.com>
2026-01-13 23:08:25 +0700

    refactor: remove old source

--- karabiner/sources/cmd/get_next_composite_key.ts
+++
@@ -1,5 +0,0 @@
-import * as compositeKey from "../consts/composite_key";
-
-Object.keys(compositeKey).forEach((key) => {
-  console.log(compositeKey[key]);
-});
--- karabiner/sources/rules.ts
+++
@@ -1,6 +0,0 @@
-import { Rule } from "./types/rule";
-
-const rules: Rule[] = [];
-
-export const addRules = (news: Rule[]) => rules.push(...news);
-export const getRules = () => rules;
